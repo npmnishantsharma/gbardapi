b12ac585f4e551634a8d090f35a8e89c97ff056a
Nishant Sharma <nishantapps55@gmail.com>
2023-08-20 21:24:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const { DiscussServiceClient } = require("@google-ai/generativelanguage");
-  
 const { GoogleAuth } = require("google-auth-library");
+
 const client = new DiscussServiceClient({
-    authClient: new GoogleAuth().fromAPIKey("AIzaSyBbvBq4Ha6ZeKKjvxvnqzGKqcGXmdRQ6T8"),
-  });
-app.get("/",async (req,res) =>{
-  const {question} = req.params;
+  authClient: new GoogleAuth().fromAPIKey("AIzaSyBbvBq4Ha6ZeKKjvxvnqzGKqcGXmdRQ6T8"),
+});
+
+app.get("/", async (req, res) => {
   const headers = req.headers;
-  console.log(headers['text'])
+  console.log(headers['text']);
+
   try {
     const result = await client.generateMessage({
       model: "models/chat-bison-001",
@@ -19,42 +20,32 @@ app.get("/",async (req,res) =>{
         messages: [{ content: `${headers['text']}` }],
       },
     });
-    const responseContent = result[0].candidates[0].content;
-    if (responseContent.startsWith("[") && responseContent.endsWith("]")) {
-      
-  const apiKey = 'AIzaSyAYK7xIpUm5QWueWH9Jd8cUlCwWFrUt2sc'; // Replace with your API key
-  const searchUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=d463665a7ac074f14&q=${responseContent}&searchType=image`;
-    
-  try {
-    const response = await axios.get(searchUrl);
-    const imageResults = response.data.items.map(item => item.link);
-    res.status(201).json({ response: imageResults[0]});
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    res.status(500).json({ error: 'An error occurred while fetching images.' });
-  }
-      
-      // You can use the 'firstData' here for further processing or searching
-      // For now, I'll just send it as a JSON response
-      
-    } else {
-      const replacedContent = responseContent.replace(/\[([^\]]+)\]/g, (match) => {
-       const apiKey1 = 'AIzaSyAYK7xIpUm5QWueWH9Jd8cUlCwWFrUt2sc'; // Replace with your API key
-       const searchUrl1 = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=d463665a7ac074f14&q=${match}&searchType=image`;
-        let i ="";
+
+    let responseContent = result[0].candidates[0].content;
+    const matches = responseContent.match(/\[([^\]]+)\]/g);
+
+    if (matches) {
+      for (const match of matches) {
+        const searchQuery = match.slice(1, -1); // Removing brackets [ ]
+        const apiKey = 'AIzaSyAYK7xIpUm5QWueWH9Jd8cUlCwWFrUt2sc';
+        const searchUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=d463665a7ac074f14&q=${searchQuery}&searchType=image`;
+
         try {
-    const response = await axios.get(searchUrl);
-    const imageResults = response.data.items.map(item => item.link);
-    i=imageResults[0];
-  } catch (error) {
-    console.error('Error fetching images:', error);
-  }
-        return `${i}`
-    });
-      res.status(201).json({ response: responseContent });
+          const response = await axios.get(searchUrl);
+          const imageResults = response.data.items.map(item => item.link);
+          const randomImage = imageResults[Math.floor(Math.random() * imageResults.length)];
+          responseContent = responseContent.replace(match, randomImage);
+        } catch (error) {
+          console.error('Error fetching images:', error);
+        }
+      }
     }
+
+    res.status(201).json({ response: responseContent });
   } catch (error) {
     console.error("Error generating message:", error);
+    res.status(500).json({ error: "An error occurred while generating the message." });
   }
 });
+
 app.listen(3000);
