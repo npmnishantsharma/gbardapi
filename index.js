822fcd6baa64e6795b78218000281b00d75f9b2c
Nishant Sharma <nishantapps55@gmail.com>
2023-08-20 21:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const { DiscussServiceClient } = require("@google-ai/generativelanguage");
   
 const { GoogleAuth } = require("google-auth-library");
 const client = new DiscussServiceClient({
-    authClient: new GoogleAuth().fromAPIKey("AIzaSyBbvBq4Ha6ZeKKjvxvnqzGKqcGXmdRQ6T8"),
-  });
-app.get("/",async (req,res) =>{
-  const {question} = req.params;
+  authClient: new GoogleAuth().fromAPIKey("YOUR_GENERATED_API_KEY"),
+});
+
+app.get("/", async (req, res) => {
   const headers = req.headers;
-  console.log(headers['text'])
+  console.log(headers['text']);
+  
   try {
     const result = await client.generateMessage({
       model: "models/chat-bison-001",
@@ -19,29 +20,37 @@ app.get("/",async (req,res) =>{
         messages: [{ content: `${headers['text']}` }],
       },
     });
-    const responseContent = result[0].candidates[0].content;
+    
+    let responseContent = result[0].candidates[0].content;
+    
     if (responseContent.startsWith("[") && responseContent.endsWith("]")) {
+      const apiKey = 'YOUR_GOOGLE_CUSTOM_SEARCH_API_KEY';
+      const matches = responseContent.match(/\[([^\]]+)\]/g);
       
-  const apiKey = 'AIzaSyAYK7xIpUm5QWueWH9Jd8cUlCwWFrUt2sc'; // Replace with your API key
-  const searchUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=d463665a7ac074f14&q=${responseContent}&searchType=image`;
-
-  try {
-    const response = await axios.get(searchUrl);
-    const imageResults = response.data.items.map(item => item.link);
-    res.status(201).json({ response: imageResults[0]});
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    res.status(500).json({ error: 'An error occurred while fetching images.' });
-  }
+      if (matches) {
+        for (const match of matches) {
+          const searchQuery = match.slice(1, -1); // Removing brackets [ ]
+          const searchUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=d463665a7ac074f14&q=${searchQuery}&searchType=image`;
+          
+          try {
+            const response = await axios.get(searchUrl);
+            const imageResults = response.data.items.map(item => item.link);
+            const randomImage = imageResults[Math.floor(Math.random() * imageResults.length)];
+            responseContent = responseContent.replace(match, randomImage);
+          } catch (error) {
+            console.error('Error fetching images:', error);
+          }
+        }
+      }
       
-      // You can use the 'firstData' here for further processing or searching
-      // For now, I'll just send it as a JSON response
-      
+      res.status(201).json({ response: responseContent });
     } else {
       res.status(201).json({ response: responseContent });
     }
   } catch (error) {
     console.error("Error generating message:", error);
+    res.status(500).json({ error: "An error occurred while generating the message." });
   }
 });
+
 app.listen(3000);
