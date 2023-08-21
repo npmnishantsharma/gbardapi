068bab046ab74e12cb1a4d4cbbcb5dc27d66ab7a
Nishant Sharma <nishantapps55@gmail.com>
2023-08-21 15:13:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
+
+const client = new DiscussServiceClient({
+
+    authClient: new GoogleAuth().fromAPIKey("AIzaSyBbvBq4Ha6ZeKKjvxvnqzGKqcGXmdRQ6T8"),
+
+  });
 app.get('/', (req, res) => {
   res.json({ response: 'API is down. Please try again later' });
 });
