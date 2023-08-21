b58aa2a04b880ef91d1cda56d57b0a9e3d818385
Nishant Sharma <nishantapps55@gmail.com>
2023-08-21 14:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,11 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
-const { DiscussServiceClient } = require("@google-ai/generativelanguage");
-  
-const { GoogleAuth } = require("google-auth-library");
-const client = new DiscussServiceClient({
-    authClient: new GoogleAuth().fromAPIKey("AIzaSyBbvBq4Ha6ZeKKjvxvnqzGKqcGXmdRQ6T8"),
-  });
-app.get("/",async (req,res) =>{
-  const {question} = req.params;
-  const headers = req.headers;
-  console.log(headers['text'])
-  try {
-    const result = await client.generateMessage({
-      model: "models/chat-bison-001",
-      prompt: {
-        context: "Respond to all questions using image",
-        messages: [{ content: `${headers['text']}` }],
-      },
-    });
-    const responseContent = result[0].candidates[0].content;
-    if (responseContent.startsWith("[") && responseContent.endsWith("]")) {
-      
-  const apiKey = 'AIzaSyAYK7xIpUm5QWueWH9Jd8cUlCwWFrUt2sc'; // Replace with your API key
-  const searchUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=d463665a7ac074f14&q=${responseContent}&searchType=image`;
+const port = 3000;
 
-  try {
-    const response = await axios.get(searchUrl);
-    const imageResults = response.data.items.map(item => item.link);
-    res.status(201).json({ response: imageResults[0]});
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    res.status(500).json({ error: 'An error occurred while fetching images.' });
-  }
-      
-      // You can use the 'firstData' here for further processing or searching
-      // For now, I'll just send it as a JSON response
-      
-    } else {
-      res.status(201).json({ response: responseContent });
-    }
-  } catch (error) {
-    console.error("Error generating message:", error);
-  }
+app.get('/', (req, res) => {
+  res.json({ response: 'API is down. Please try again later' });
 });
-app.listen(3000);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
