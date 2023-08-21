aed29acd747542832472aedaf30cd2e98b7b2053
Nishant Sharma <nishantapps55@gmail.com>
2023-08-21 18:22:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const { DiscussServiceClient } = require("@google-ai/generativelanguage");
-const { GoogleAuth } = require("google-auth-library");
 
-const client = new DiscussServiceClient({
-
-    authClient: new GoogleAuth().fromAPIKey("AIzaSyBbvBq4Ha6ZeKKjvxvnqzGKqcGXmdRQ6T8"),
-
-  });
-app.get('/', async (req, res) => {
-    const headers = req.headers;
-    if(headers['text'] == null) return;
-    try {
-    const result = await client.generateMessage({
-
-      model: "models/chat-bison-001",
-
-      prompt: {
-
-        context: "Respond to all questions in a paragraph. ",
-
-        messages: [{ content: `${headers['text']}` }],
-
-      },
-
-    });
-
-    const responseContent = result[0].candidates[0].content;
-    res.json({ response: `${responseContent}` });
-    }catch (error) {
-        res.json({ response: `Sometimes something gets wrong!` });
-    console.error("Error generating message:", error);
-
-  }
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Vercel!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
 module.exports = app;
