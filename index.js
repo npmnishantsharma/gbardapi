cb92463c714f8747cde50441c64990eed78ba04a
Nishant Sharma <nishantapps55@gmail.com>
2023-08-21 15:12:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
 app.get('/', (req, res) => {
   res.json({ response: 'API is down. Please try again later' });
 });
